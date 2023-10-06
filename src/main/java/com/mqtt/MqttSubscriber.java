89aa1e2d7137baeaa1a489325c680dd6616b4f9a
김나영` <knyg99@naver.com>
2023-10-06 17:03:31 +0900

    update: mqttsubscriber class
    콜백이아닌 spring으로 변경
    
    ref: #1

--- src/main/java/com/mqtt/MqttSubscriber.java
+++ src/main/java/com/mqtt/MqttSubscriber.java
@@ -3,15 +3,14 @@ package com.mqtt;
 
 
 import org.eclipse.paho.client.mqttv3.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
-import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.integration.mqtt.support.MqttHeaders;
@@ -19,55 +18,63 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
 
+@Configuration
+@EnableIntegration
+public class MqttSubscriber {
 
-public class MqttSubscriber implements MqttCallback {
 
-    private static final String BROKER_URL = "tcp://localhost:1883";
+
+    private static final String BROKER_URL = System.getenv("url");
     private static final String MQTT_CLIENT_ID = MqttAsyncClient.generateClientId();
-    private static final String TOPIC_FILTER = "topic";
-    private static final String MQTT_USERNAME = "username";
-    private static final String MQTT_PASSWORD = "pwd";
+    private static final String TOPIC_FILTER = "application";
+    private static final String MQTT_USERNAME = System.getenv("username");
+    private static final String MQTT_PASSWORD = System.getenv("password");
 
     public static void main(String[] args) {
-        try {
-            // MQTT 연결 옵션 설정
-            MqttConnectOptions options = new MqttConnectOptions();
-            options.setCleanSession(true);
-            options.setUserName(MQTT_USERNAME);
-            options.setPassword(MQTT_PASSWORD.toCharArray());
-
-            // MQTT 클라이언트 생성
-            MqttClient client = new MqttClient(BROKER_URL, MQTT_CLIENT_ID);
-            client.setCallback(new MqttSubscriber());
-
-            // 연결
-            client.connect(options);
-
-            // 구독
-            client.subscribe(TOPIC_FILTER);
-
-            System.out.println("MQTT Subscriber가 시작되었습니다. 토픽: " + TOPIC_FILTER);
-
-
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
+        new SpringApplicationBuilder(MqttSubscriber.class)
+                .run(args);
     }
 
-    @Override
-    public void connectionLost(Throwable throwable) {
-        System.out.println("연결이 끊겼습니다.");
+
+    private MqttConnectOptions connectOptions() {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(true);
+        options.setUserName(MQTT_USERNAME);
+        options.setPassword(MQTT_PASSWORD.toCharArray());
+        return options;
     }
 
-    @Override
-    public void messageArrived(String topic, MqttMessage message) {
-        System.out.println("=====================메세지 도착=================");
-        System.out.println("Topic: " + topic);
-        System.out.println("Message: " + new String(message.getPayload()));
+    @Bean
+    public DefaultMqttPahoClientFactory defaultMqttPahoClientFactory() {
+        DefaultMqttPahoClientFactory clientFactory = new DefaultMqttPahoClientFactory();
+        clientFactory.setConnectionOptions(connectOptions());
+        return clientFactory;
+    }
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
     }
 
-    @Override
-    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-        System.out.println("메시지 전달 완료.");
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(BROKER_URL, MQTT_CLIENT_ID, TOPIC_FILTER);
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
     }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler inboundMessageHandler() {
+        return message -> {
+            String topic = (String) message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC);
+            System.out.println("Topic:" + topic);
+            System.out.println("Payload" + message.getPayload());
+        };
+    }
+
+
 }
